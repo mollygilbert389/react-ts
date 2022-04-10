72c5a015034609c532e1bc9e1717115a851da6dc
Molly <molly@oleaedge.com>
2022-04-10 15:17:09 -0500

    adding fetch api call

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <React.Fragment>
-            <div aria-label="footer" style={{ backgroundColor: "black", color: "white", padding: 20, borderRadius: 5 }}>
+            <div aria-label="footer" style={{ backgroundColor: "black", color: "white", padding: 20, borderRadius: 5, position: "absolute", left: 0, bottom: 0, right: 0 }}>
                 I am footer!
             </div>
         </React.Fragment>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,40 +1,22 @@
 import React from "react";
-// import { Page, PageProps } from "./layout";
-// import { Loading, PageError } from "components";
-// import { useGetUserBoardsQuery } from "@molly/apollo";
 
 type Props = {
     id: string;
 }
 
-const BoardContainer: React.FC<Props> = ({ id }) => {
-  // const { data, loading, error } = useGetUserBoardsQuery({ varibles: { id }})
+const BoardContainer: React.FC<Props> = () => {
 
-  // if(loading && data === undefined) {
-  //   return <Loading/>
-  // } else if (error) {
-  //   return <PageError error={error} />
-  // } else {
+  React.useEffect(() => {
+    fetch('https://api.artic.edu/api/v1/artworks')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }, [])
+
   return (
-      <React.Fragment>
-        {/* <Container>
-          
-        </Container> */}
-         Hello World
-      </React.Fragment>
-    )
-  // }
+    <React.Fragment>
+        Hello World
+    </React.Fragment>
+  )
 }
 
 export default BoardContainer;
-
-// const HomePage: React.FC<PageProps> = ({}) => {
-//   const id = params.id;
-//   return (
-//       <Page>
-//           <BoardContainer id={id}/>
-//       </Page>
-//   )
-// }
-
-// export default HomePage;
